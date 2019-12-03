6d277b06490c33977f5b205f72ddbcc970a5d34f
abdalkreemorabi <abdalkreemorabi11@gmail.com>
2019-12-03 23:34:27 +0200

    EmptyTodoList

---
+++ src/component/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodosLists extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome todosList component</p>
+            </div>
+        )
+    }
+}
